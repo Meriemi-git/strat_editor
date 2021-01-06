91a1600d9666d356e76c93396816e169c383679c
Remi PLANTADE <remi.plantade@neos-sdi.com>
2021-01-06 22:38:47 +0100

    Add authent filter on images route

--- strat-editor/apps/backend/src/app/gallery/gallery.controller.ts
+++ strat-editor/apps/backend/src/app/gallery/gallery.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { GalleryService } from './gallery.service';
 
 @Controller('gallery')
 export class GalleryController {
   constructor(private readonly galleryService: GalleryService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('images')
   allImages(@Body() userId): Promise<string[]> {
     return this.galleryService.getAllImagesForUser(userId);
